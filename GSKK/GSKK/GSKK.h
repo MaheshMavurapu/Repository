6d8a88dc93bf6d24cc3652b2f45238e792a96097
MaheshMavurapu <mahesh.mavurapu@gmail.com>
2019-01-07 00:22:10 +0530

    GSKK project addded successfully

---
+++ GSKK/GSKK/GSKK.h
@@ -0,0 +1,19 @@
+//
+//  GSKK.h
+//  GSKK
+//
+//  Created by Mahesh Mavurapu on 07/01/19.
+//  Copyright © 2019 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSKK.
+FOUNDATION_EXPORT double GSKKVersionNumber;
+
+//! Project version string for GSKK.
+FOUNDATION_EXPORT const unsigned char GSKKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSKK/PublicHeader.h>
+
+
--- GSKK/GSKK/Mahesh.swift
+++ GSKK/GSKK/Mahesh.swift
@@ -1,8 +1,8 @@
 //
-//  File.swift
-//  GSK
+//  Mahesh.swift
+//  GSKK
 //
-//  Created by Mahesh Mavurapu on 06/01/19.
+//  Created by Mahesh Mavurapu on 07/01/19.
 //  Copyright © 2019 Mahesh Mavurapu. All rights reserved.
 //
 
