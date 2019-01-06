c0580ee4b88c011b1cb762743a5671bf5cadaec7
MaheshMavurapu <mahesh.mavurapu@gmail.com>
2019-01-06 23:36:06 +0530

    Initial Commit

---
+++ GSK/GSK/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  GSK
+//
+//  Created by Mahesh Mavurapu on 06/01/19.
+//  Copyright © 2019 Mahesh Mavurapu. All rights reserved.
+//
+
+import Foundation
---
+++ GSK/GSK/GSK.h
@@ -0,0 +1,19 @@
+//
+//  GSK.h
+//  GSK
+//
+//  Created by Mahesh Mavurapu on 06/01/19.
+//  Copyright © 2019 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSK.
+FOUNDATION_EXPORT double GSKVersionNumber;
+
+//! Project version string for GSK.
+FOUNDATION_EXPORT const unsigned char GSKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSK/PublicHeader.h>
+
+
